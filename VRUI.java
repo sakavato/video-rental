6afebe423990a5e44ca5e02f077d5b4ae8d3aeb0
stellakang <kangsj1235@gmail.com>
2023-02-17 15:34:11 +0900

    VRUI.java: separate input from register()

--- VRUI.java
+++ VRUI.java
@@ -180,26 +180,33 @@ public class VRUI {
 		customerRentals.add(rental);
 		foundCustomer.setRentals(customerRentals);
 	}
+	public Video createVideo(){
+		System.out.println("Enter video title to register: ") ;
+		String title = scanner.next() ;
 
+		System.out.println("Enter video type( 1 for VHD, 2 for CD, 3 for DVD ):") ;
+		int videoType = scanner.nextInt();
+
+		System.out.println("Enter price code( 1 for Regular, 2 for New Release ):") ;
+		int priceCode = scanner.nextInt();
+
+		Date registeredDate = new Date();
+		Video video = new Video(title, videoType, priceCode, registeredDate) ;
+		return video;
+	}
+	public Customer createCustomer(){
+		System.out.println("Enter customer name: ") ;
+		String name = scanner.next();
+		Customer customer = new Customer(name) ;
+		return customer;
+	}
 	public void register(String object) {
 		if ( object.equals("customer") ) {
-			System.out.println("Enter customer name: ") ;
-			String name = scanner.next();
-			Customer customer = new Customer(name) ;
+			Customer customer = createCustomer();
 			customers.add(customer) ;
 		}
 		else {
-			System.out.println("Enter video title to register: ") ;
-			String title = scanner.next() ;
-
-			System.out.println("Enter video type( 1 for VHD, 2 for CD, 3 for DVD ):") ;
-			int videoType = scanner.nextInt();
-
-			System.out.println("Enter price code( 1 for Regular, 2 for New Release ):") ;
-			int priceCode = scanner.nextInt();
-
-			Date registeredDate = new Date();
-			Video video = new Video(title, videoType, priceCode, registeredDate) ;
+			Video video = createVideo();
 			videos.add(video) ;
 		}
 	}
